e6a5baa7d0c1e973d124a551ee0cc8e6fa41a44f
Tyuiu.ArtemihinND <nikitos.artemihin@mail.ru>
2025-06-02 03:31:59 +0500

    Add project files.

---
+++ Tyuiu.ArtemihinND.Sprint1.Task0.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ArtemihinND.Sprint1.Task0.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ArtemihinND.Sprint1.Task0.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6757c5a7-2e77-480c-8af9-dc55f8c77fef")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
